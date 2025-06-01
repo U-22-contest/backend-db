de7290b88f2a6021f90398f3db44ccb5778aefe2
atsuya <f.atsuya.personal@gmail.com>
2025-06-01 23:19:26 +0900

    複数のCategory検索で絞れるように

--- nest-backend/src/novels/dto/search-novels.dto.ts
+++ nest-backend/src/novels/dto/search-novels.dto.ts
@@ -1,5 +1,18 @@
+import { IsArray, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class SearchNovelsDto {
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @IsOptional()
+  @IsString()
   author?: string;
-  category?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+  categories?: string[];
 }
