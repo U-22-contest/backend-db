b5532771eadafcc2d12932710bb488a97210be48
S-kit0408 <suzukikit1688@gmail.com>
2025-07-24 16:45:05 +0900

    小説変更後データを返す

--- nest-backend/src/novels/services/edit-novel-by-id.service.ts
+++ nest-backend/src/novels/services/edit-novel-by-id.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { EditNovelsDto } from '../dto/request/edit-novel.dto';
+import { EditNovelsDto, EditNovelsResponse } from '../dto/request/edit-novel.dto';
 import { postgresEditNovelRepository } from '../repositories/edit-novel-by-id/postgres';
 import { MongoEditNovelRepository } from '../repositories/edit-novel-by-id/mongo';
+import { PostgresGetNovelByIdRepository } from '../repositories/get-novel-by-id/postgres';
+import { MongoGetNovelByIdRepository } from '../repositories/get-novel-by-id/mongo';
 
 @Injectable()
 export class EditNovelService {
   constructor(
     private readonly postgresEditNovel: postgresEditNovelRepository,
     private readonly mongoEditNovel: MongoEditNovelRepository,
+    private readonly postgresGetNovelById: PostgresGetNovelByIdRepository,
+    private readonly mongoGetNovelById: MongoGetNovelByIdRepository,
   ) {}
 
   async editNovelById(
@@ -15,29 +19,29 @@ export class EditNovelService {
     userId: string,
     editDto: EditNovelsDto,
     // returnする型は編集した結果を返すべき
-  ): Promise<{ message: string }> {
+  ): Promise<EditNovelsResponse> {
     const novel = await this.postgresEditNovel.findByAuthor(id, userId);
     if (!novel) {
       throw new Error('該当小説がありません');
     }
 
     // titleとcategories、coverImagePathの編集
-    if ( editDto.title || editDto.categories || editDto.coverImagePath ) {
-      await this.postgresEditNovel.updateNovel(id, editDto)
-    }
+    const updatedPostgresNovel = ( editDto.title || editDto.categories || editDto.coverImagePath )
+      ? await this.postgresEditNovel.updateNovel(id, editDto)
+      : novel;
 
     // contentとoverviewの編集
-    if (editDto.content || editDto.overview) {
-      await this.mongoEditNovel.updateContentBySharedId(
-        novel.sharedId,
-        editDto,
-      );
-    }
+    const updatedMongoNovel = (editDto.content || editDto.overview)
+      ? await this.mongoEditNovel.updateContentBySharedId(novel.sharedId, editDto)
+      : await this.mongoGetNovelById.findBySharedId(novel.sharedId)
+
 
     // TODO: 編集した内容を返す必要あり
-    // 全部返す？それとも変更した箇所だけ？
+    // → 小説全体を返す。
     return {
-      message: '小説を更新しました',
+      ...updatedPostgresNovel,
+      overview: updatedMongoNovel?.overview,
+      content: updatedMongoNovel?.content,
     };
   }
 }
